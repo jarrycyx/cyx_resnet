9f1cc6d85beb344cf22f022619ba3a4db63190e9
jarrycyx <dzdzzd@126.com>
2020-11-26 11:45:12 +0800

    MultiThread

---
+++ MultiThreadTrain.py
@@ -0,0 +1,44 @@
+from TrainBagging import TrainBag
+import threading
+from Utils.LogUtils import Log
+
+class TrainThread (threading.Thread):
+    def __init__(self, name, trainbag):
+        threading.Thread.__init__(self)
+        self.name = name
+        self.trainbag = trainbag
+        
+    def run(self):
+        log.printlog("Start Thread: " + self.name)
+        init_train(self.trainbag, self.name)
+        log.printlog ("Exit Thread: " + self.name)
+
+def init_train(trainbag, name):
+    for i in range(EPOCH_NUM):
+        log.printlog("Epoch: {:d}/{:d} ({:s})".format(i, EPOCH_NUM, name))
+        trainbag.train_step(show_every=200)
+        trainbag.val_step()
+        if (i+1) % int(EPOCH_NUM/4) == 0:
+            torch.save(trainbag.resnet.state_dict(),"./pklmodels/"+name+"_epoch_"+str(i+1)+".pkl")
+            log.printlog("Saving state pkls:" + name)
+        
+
+CUDA_DEVICE = [2,3,3]
+DESCRIPTIONS = ["Class100_A", "Class100_B", "Class100_C"] # different descriptions
+BAGS_NPY = ["bagging/bag1.npy", "bagging/bag2.npy", "bagging/bag3.npy"]
+EPOCH_NUM = 40
+log = Log(clear=True)
+
+trainbags = []
+threads = []
+
+for i in range(len(CUDA_DEVICE)):
+    trainbags.append(TrainBag("q1_data/train2.csv", "q1_data/train.npy", BAGS_NPY[i], "bagging/val.npy", logUtil=log, cuda_device=CUDA_DEVICE[i], description=DESCRIPTIONS[i]))
+    trainbags[i].load_net(epoch_num=EPOCH_NUM)
+    threads.append(TrainThread(DESCRIPTIONS[i], trainbags[i]))
+    threads[i].start()
+
+for thread in threads:
+    thread.join()
+    
+log.printlog("Threads Ended")
--- TrainBagging.py
+++ TrainBagging.py
@@ -21,7 +21,7 @@ class TrainBag(object):
     CUDA_DEVICE_IDX = 2
     LR = 0.00002
     CLASS_NUM = 100
-    BATCH_SIZE = 50
+    BATCH_SIZE = 30
     LOGPATH = "resnet_train.log"
     WEIGHT_DECAY = 0
 
@@ -32,10 +32,11 @@ class TrainBag(object):
         if not self.log == None:
             self.log.printlog(str)
     
-    def __init__(self, csv_path, dataset_path, bag_refer_list, val_refer_list, logUtil=None, cuda_device=2):
+    def __init__(self, csv_path, dataset_path, bag_refer_list, val_refer_list, logUtil=None, cuda_device=2, description=""):
         self.log = logUtil
         self.printlog("Current PID: " + str(os.getpid()))
         self.device = cuda_device
+        self.description = description
         
         TrainDataset = DataUtils.DatasetLoader(csv_path, dataset_path, refer_list=np.load(bag_refer_list),
                                                mode="Train", up_size=self.UP_SIZE)
@@ -82,8 +83,8 @@ class TrainBag(object):
             accuracy = np.mean(label.numpy()==y_class)
             
             if i % show_every == 0:
-                self.printlog("Batch: {:d}/{:d} loss: {:.4f} accuracy: {:.4f} lr: {:.7f}" 
-                     .format(i, len(self.trainloader), loss, accuracy, this_LR))
+                self.printlog("Batch: {:d}/{:d} loss: {:.4f} accuracy: {:.4f} lr: {:.7f} ({:s})" 
+                     .format(i, len(self.trainloader), loss, accuracy, this_LR, self.description))
         
         
         self.variableLR.step()
@@ -104,29 +105,31 @@ class TrainBag(object):
             # if 0:
             #     cv2.imwrite("imgs/test.jpg", cv2.cvtColor(val_x[0].transpose(1,2,0), cv2.COLOR_RGB2BGR))    
             
-        self.printlog("Val Accuracy: {:.4f}".format(np.array(accuracy).mean()))
+        self.printlog("Val Accuracy: {:.4f} ({:s})".format(np.array(accuracy).mean(), self.description))
     
 
-CUDA_DEVICE = [3,2,2]
-DESCRIPTIONS = ["Class100_A", "Class100_B", "Class100_C"] # different descriptions
-EPOCH_NUM = 40
-log = Log(clear=True)
+if __name__ == "__main__":
 
-trainbags = []
+    CUDA_DEVICE = [2,2,2]
+    DESCRIPTIONS = ["Class100_A", "Class100_B", "Class100_C"] # different descriptions
+    EPOCH_NUM = 40
+    log = Log(clear=True)
 
-trainbags.append(TrainBag("q1_data/train2.csv", "q1_data/train.npy", "bagging/bag1.npy", "bagging/val.npy", logUtil=log, cuda_device=CUDA_DEVICE[0]))
-trainbags.append(TrainBag("q1_data/train2.csv", "q1_data/train.npy", "bagging/bag2.npy", "bagging/val.npy", logUtil=log, cuda_device=CUDA_DEVICE[1]))
-trainbags.append(TrainBag("q1_data/train2.csv", "q1_data/train.npy", "bagging/bag3.npy", "bagging/val.npy", logUtil=log, cuda_device=CUDA_DEVICE[2]))
+    trainbags = []
 
-# trainbags.append(TrainBag("q1_data/train1.csv", "q1_data/train.npy", "bagging/train_list.npy", "bagging/val_list.npy", log))
-for j in range(len(trainbags)):
-    trainbags[j].load_net(epoch_num=EPOCH_NUM)
-    
-for i in range(EPOCH_NUM):
+    trainbags.append(TrainBag("q1_data/train2.csv", "q1_data/train.npy", "bagging/bag1.npy", "bagging/val.npy", logUtil=log, cuda_device=CUDA_DEVICE[0], description=DESCRIPTIONS[0]))
+    trainbags.append(TrainBag("q1_data/train2.csv", "q1_data/train.npy", "bagging/bag2.npy", "bagging/val.npy", logUtil=log, cuda_device=CUDA_DEVICE[1], description=DESCRIPTIONS[1]))
+    trainbags.append(TrainBag("q1_data/train2.csv", "q1_data/train.npy", "bagging/bag3.npy", "bagging/val.npy", logUtil=log, cuda_device=CUDA_DEVICE[2], description=DESCRIPTIONS[2]))
+
+    # trainbags.append(TrainBag("q1_data/train1.csv", "q1_data/train.npy", "bagging/train_list.npy", "bagging/val_list.npy", log))
     for j in range(len(trainbags)):
-        log.printlog("Bag: {:d} Epoch: {:d}/{:d}".format(j, i, EPOCH_NUM))
-        trainbags[j].train_step(show_every=100)
-        trainbags[j].val_step()
-        if (i+1) % int(EPOCH_NUM/4) == 0:
-            torch.save(trainbags[j].resnet.state_dict(),"./pklmodels/"+DESCRIPTIONS[j]+"_epoch_"+str(i+1)+".pkl")
-            log.printlog("Saving state pkls")
+        trainbags[j].load_net(epoch_num=EPOCH_NUM)
+        
+    for i in range(EPOCH_NUM):
+        for j in range(len(trainbags)):
+            log.printlog("Bag: {:d} Epoch: {:d}/{:d}".format(j, i, EPOCH_NUM))
+            trainbags[j].train_step(show_every=100)
+            trainbags[j].val_step()
+            if (i+1) % int(EPOCH_NUM/4) == 0:
+                torch.save(trainbags[j].resnet.state_dict(),"./pklmodels/"+DESCRIPTIONS[j]+"_epoch_"+str(i+1)+".pkl")
+                log.printlog("Saving state pkls")
